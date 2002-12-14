4c5bf55c2acd4cba63a60044f8ebf945f51a7083
Tim Ansell <tim@thousandparsec.net>
2002-12-14 10:47:23 +0000

    More php updates.

--- index.php
+++ index.php
@@ -1,28 +1,16 @@
 <?php include "bits/start_page.inc" ; ?>
 
-<?php include "bits/start_section.inc" ; ?>
-<p>
-TP is a 4 X's game (eXplore, eXpand, eXterminate, eXploit) but it is diffrent
-in the sense that it is designed for long games supporting universes as large 
-as your computer can handle. (Have a quad Xeon with 4 gig of ram? Well have a
-couple of million systems in your universe! :). It also supports long term 
-play with players joing and leaving throughout the game. As well the tech tree 
-is easily expandable and can be extended during the game without effecting 
-anybody already playing (ie introduction of new technology as the game 
-progresses).
-</p><p>
-You can choose to have highly exotic universes where the laws of physics are 
-totally diffrent or universes that are very similar to our own.
-</p><p>
-The game is TURN BASED, this means that clicking quickly isn't vital but 
-stratergy is.
-</p><p>
-The game should also be highly extendable. Allowing player customisation not 
-found in the commercial games.
-</p><p>
-Anyway we have high hopes for what TP will become. We have currently stolen 
-alot of ideas from Stars!, VGA Planets an XPace.
-</p>
-<?php include "bits/end_section.inc" ; ?>
+
+<?php
+
+$d = dir('news');
+
+while ($file = $this_dir->read()) {
+	include "bits/start_section.inc";
+	include $file;
+	include "bits/end_section.inc";
+}
+
+?>
 
 <?php include "bits/end_page.inc" ; ?>
