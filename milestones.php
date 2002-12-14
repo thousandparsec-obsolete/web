b246c0f237b5888ead804eebd108d3318afa69e7
Tim Ansell <tim@thousandparsec.net>
2002-12-14 11:25:06 +0000

    Updated milestones to use php.

---
+++ milestones.php
@@ -0,0 +1,174 @@
+<?php include "bits/start_page.inc" ?>
+
+<?php include "bits/start_section.inc" ?>
+
+<h1>Milestones</h1>
+	
+<p>After each milestone is reached the CVS should be tagged and a tarball created.</p>
+			
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<h2>Alpha</h2>
+<b>Goals:</b>
+<ul>
+	<li>Client can connect to server</li>
+	<li>Client can login with a username/password</li>
+</ul>
+<b>Documentation:</b>
+<ul>
+	<li>Simple protocol definition</li>
+	<li>Order/Goals definition</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<h2>Bravo</h2>
+<b>Goals:</b>
+<ul>
+	<li>Client can "see" a universe filled with "dummy" objects</li>
+</ul>
+<b>Documentation:</b>
+<ul>
+	<li>Object types definition</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<h2>Charlie</h2>
+<b>Goals:</b>
+<ul>
+	<li>Client can issue a "dummy" order to "dummy" objects</li>
+	<li>Client can remove orders from objects</li>
+	<li>Objects define which orders can be performed on themselves</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Delta</h2>
+<b>Goals:</b>
+<ul>
+	<li>A "planet" object exists</li>
+	<li>A "fleet" object exists</li>
+	<li>A "move" order exists</li>
+	<li>The "move" order can be issued to the "fleet" object</li>
+	<li>The order returns how long it takes to get to the destination</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Echo</h2>
+<b>Goals:</b>
+<ul>
+	<li>The move order actually takes effect and "fleet" objects can move about the universe</li>
+	<li>Orders are removed when completed</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Foxtrot</h2>
+<b>Goals:</b>
+<ul>
+	<li>A "build" order exists</li>
+	<li>The build order can produce "fleet" objects</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Golf</h2>
+<b>Goals:</b>
+<ul>
+	<li>"Design" objects exist</li>
+	<li>"Componet" objects exists</li>
+	<li>Engine componets exists</li>
+	<li>Clients can build designs out of componets</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Hotel</h2>
+<b>Goals:</b>
+<ul>
+	<li>Planets gain stats</li>
+	<li>"Fleet" objects become a set of attributes and references to design object</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>India</h2>
+<b>Goals:</b>
+<ul>
+	<li>Start of a simple economy.</li>
+	<li>Planet stats change how fast/slow ships are built</li>
+	<li>Building ships uses resources</li>
+	<li>Ships componets effect how fast/slow they move</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Juliet</h2>
+<b>Goals:</b>
+<ul>
+	<li>People now "own" planets</li>
+	<li>Colonise order exists which allows unowned planets to have ownership gained</li>
+	<li>Orders only issued to "own" planets</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Kilo</h2>
+<b>Goals:</b>
+<ul>
+	<li>Transport orders exist and allow pick up and deliver of resources</li>
+	<li>Cargo hold componet is created and effects how much resources can be pickedup delivered</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Lima</h2>
+<b>Goals:</b>
+<ul>
+	<li>Weapons componets are added</li>
+	<li>Attack order exists</li>
+	<li>Ships can have battles</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>Mike</h2>
+<b>Goals:</b>
+<ul>
+	<li>Technology can be researched</li>
+	<li>Tech restricts which componets are avalible</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>November</h2>
+<b>Goals:</b>
+<ul>
+	<li>Add a whole bunch of componets to make the game fun</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+	
+<h2>After</h2>
+<p>Continue with development adding features and flesh out the game.</p>
+
+<?php include "bits/end_section.inc" ?>
+
+<?php include "bits/end_page.inc" ?>
