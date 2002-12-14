db3fc7dcb18ceac082bcb7dc6d142d67a4b31daa
Tim Ansell <tim@thousandparsec.net>
2002-12-14 12:26:20 +0000

    Added links.

---
+++ links.php
@@ -0,0 +1,90 @@
+<?php $title = "Links" ?>
+
+<?php include "bits/start_page.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<p>
+	<font size="6">
+		Thousand Parsec links
+	</font>
+</p>
+<ul>
+	<li></li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<p>
+	<font size="6">
+		Universe links
+	</font>
+</p>
+<p>
+	Another open source game similar to Thousand Parsec, it's written in JAVA.
+</p>
+<ul>
+	<li>
+		<a href="http://universe.sf.net">
+			Universe at Sourceforge
+		</a>
+	</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<p>
+	<font size="6">
+		Stars! links
+	</font>
+</p>
+<p>
+	Stars! is a similar game to what we are trying to create. It's a decent 
+	game and there is a playable demo. 
+</p>
+<ul>
+	<li>
+		Way point Zero, offical Stars! home page
+	</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<p>
+	<font size="6">
+		VGA Planets links
+	</font>
+</p>
+<p>
+	VGA Planets is another similar game. There is also a shareware version.
+</p>
+<ul>
+	<li></li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<p>
+	<font size="6">
+		XPace links
+	</font>
+</p>
+<p>
+	Another commerical game like Stars! and VGA Planets. Downloadable for
+	Free because it's been discontinued. Was never finished.
+</p>
+<ul>
+	<li>
+		<a href="http://www.xoftware.com">
+			Xoftware
+		</a>, 
+          creators of XPACE
+	</li>
+</ul>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/end_page.inc" ?>
+
