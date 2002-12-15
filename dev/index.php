5a9c718b3ffb7a3917194d984ff053ad4bcf8ed4
Tim Ansell <tim@thousandparsec.net>
2002-12-15 02:53:05 +0000

    New dev section.

---
+++ dev/index.php
@@ -0,0 +1,10 @@
+<?php $title = "Developers section" ?>
+
+<?php include = "bits/start_page.inc" ?>
+<?php include = "bits/start_section.inc" ?>
+
+Nothing here yet.
+
+<?php include = "bits/end_section.inc" ?>
+<?php include = "bits/end_page.inc" ?>
+
