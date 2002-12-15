8958dc6ba379dc04c613c5547f317a692b071dcb
Tim Ansell <tim@thousandparsec.net>
2002-12-15 02:41:59 +0000

    Converted the documents page to php.

---
+++ documents.php
@@ -0,0 +1,86 @@
+<?php $title = "Documents" ?>
+
+<?php include "bits/start_page.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<blockquote> 
+	<p>
+		<font size="4">
+			Game Play
+		</font>
+	</p>
+	<p>
+		Documents about how game play works
+	</p>
+	
+	<blockquote> 
+		<p>
+			HABIBILITY
+		</p>
+		<blockquote>
+			<p>
+				Working out how many people can exist on a planet, how fast a population 
+				grows or even if the race can land this planet at all.
+			</p>
+		</blockquote>
+	</blockquote>
+	
+	<p>
+		<font size="4">
+			Clients
+		</font>
+	</p>
+	<p>
+		Documents that are specificly related with a certain client.
+	</p>
+	<blockquote> 
+		<p>
+			NONE
+		</p>
+	</blockquote>
+	
+	<p>
+		<font size="4">
+			Server
+		</font>
+	</p>
+	<p>
+		Documents about how the server works
+	</p>
+	
+	<blockquote> 
+		<p>
+			FILES
+		</p>
+		<blockquote> 
+			<p>
+				Talks about the files that the server uses.
+			</p>
+		</blockquote>
+		<p>
+			TURN
+		</p>
+		<p>
+			Talks about what happens in a turn and the order things occur.
+		</p>
+	</blockquote>
+	
+    <p>
+		<font size="4">
+			Other
+		</font>
+	</p>
+	<p>
+		Other documents
+	</p>
+	
+	<blockquote>
+		<p>
+			NONE
+		</p>
+	</blockquote>
+
+</blockquote>
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/end_page.inc" ?>
