082a762753bb5b9dbbdb2b8a529e0d5248827f68
Lee Begg <lee@thousandparsec.net>
2003-02-01 07:18:37 +0000

    Added 0.0.1 server to download page.

--- downloads.php
+++ downloads.php
@@ -4,10 +4,11 @@
 <?php include "bits/start_section.inc" ?>
 
 <p>
-	Currently there is nothing to download.
+	Currently there is not much to download.
 </p>
+<h2>Server</h2>
 <p>
-	Please come back at another time.
+	<a href="tpserv-0.0.1.tar.gz">Version 0.0.1</a> tar/gz, 92KB	
 </p>
 
 <?php include "bits/end_section.inc" ?>
