554d88057d7b10165b542113af8ddb9040876263
Tim Ansell <tim@thousandparsec.net>
2003-12-18 14:49:13 +0000

    More updates.

--- utils/sloc2html.sh
+++ utils/sloc2html.sh
@@ -4,7 +4,7 @@ CVSROOT=/var/www/www.thousandparsec.net/cvs/
 TMPDIR=/tmp/sloc2html
 
 INPUT=/tmp/sloc.output
-OUTPUT=/var/www/www.thousandparsec.net/tp/bits/sloc.inc
+OUTPUT=/var/www/www.thousandparsec.net/tp/dev/bits/sloc.inc
 
 mkdir $TMPDIR
 sloccount --wide --multiproject --datadir $TMPDIR $CVSROOT > $INPUT 2> /dev/null
