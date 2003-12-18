8648b7fa3f130c8e7dfd99494fdae7c870d7d3cd
Tim Ansell <tim@thousandparsec.net>
2003-12-18 15:00:00 +0000

    More updates.

--- dev/index.php
+++ dev/index.php
@@ -3,8 +3,26 @@
 <?php include "bits/start_page.inc" ?>
 <?php include "bits/start_section.inc" ?>
 
-This is the developers section. If you just want to use Thousand Parsec
-just click on one of the buttons in the top menu.
+<br>
+<br>
+
+<font size="4">
+		Welcome Developers!
+</font>
+
+<p>
+	This is the developers section. If you just want to use Thousand Parsec
+	just click on one of the buttons in the top menu.
+</p>
+
+<p>
+	If you would like to help out just contact mithro at 
+	<a href="mailto:developer AT thousandparsec DOT net">developer AT thousandparsec DOT net</a>
+	or see him on <a href="irc://irc.worldforge.org/#tp">irc.worldforge.org channel #tp</a>
+</p>
+
+<br>
+<br>
 
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/end_page.inc" ?>
