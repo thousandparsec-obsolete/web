66720d425efd18f3ca58e36458c02f7979b3a761
Tim Ansell <tim@thousandparsec.net>
2003-12-18 14:54:48 +0000

    More updates.

--- dev/index.php
+++ dev/index.php
@@ -3,7 +3,8 @@
 <?php include "bits/start_page.inc" ?>
 <?php include "bits/start_section.inc" ?>
 
-Nothing here yet.
+This is the developers section. If you just want to use Thousand Parsec
+just click on one of the buttons in the top menu.
 
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/end_page.inc" ?>
