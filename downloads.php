d9412712265c0beb0761af9eae0f04987a3cae36
Tim Ansell <tim@thousandparsec.net>
2004-04-19 15:29:28 +0000

    Fixing up the download page.

--- downloads.php
+++ downloads.php
@@ -5,6 +5,26 @@
 <?php include "bits/func.inc" ?>
 <?php include "bits/start_page.inc" ?>
 <?php include "bits/start_section.inc" ?>
+<?php 
+
+function display($directory) {
+
+	$dir = $downloads . $directory;
+	$files = @get_files($dir);
+
+	foreach ($files as $file) {
+		list($trash, $goodness) = split("-", $file, 2);
+		list($major, $minor, $revision, $tar, $compression) = split("\.", $goodness, 5);
+	?>
+
+<p>
+	<a href="<?php echo $dir . $file; ?>"> Version <?php echo "$major.$minor.$revision"; ?></a> <?php echo "$tar/$compression"; ?>, <?php echo (int)(filesize($dir . $file)/1024) ?> KB
+</p>
+
+<?php
+	}
+}
+?>
 
 <h2>C++ Server</h2>
 <p>
@@ -56,5 +76,24 @@ foreach ($files as $file) {
 <?php
 }
 ?>
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<h2>Python Text client</h2>
+<p>
+	This client works with any computer which has Python and py-netlib installed.<br>
+	Binaries for windows may be avalible at a later date. It doesn't have all the 
+	pretty graphics but it is the first to get new features.
+</p>
+<?php display("pytext-client"); ?>
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<h2>Python TP Network Library</h2>
+<p>
+	This library is used by all the new python clients to connect to TP servers.<br>
+	All new python clients require this library to function.
+</p>
+<?php display("pytext-client"); ?>
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/end_page.inc" ?>
