b2d200876666585c5f37f8123a6b2a343ec3e684
Tim Ansell <tim@thousandparsec.net>
2004-05-01 05:08:20 +0000

    Opps the order need a type in it. This is how the server does it.

--- dev/documents/protocol2.php
+++ dev/documents/protocol2.php
@@ -546,6 +546,7 @@ Example:
     A Order packet consist of:
     <ul>
         <li>a UInt32, Object ID of the order is on/to be placed on</li>
+        <li>a UInt32, Order Type ID</li>
         <li>a UInt32, Slot number of the order/to be put in, -1 will insert
 			at the last position, otherwise it is inserted before the number</li>
         <li>a UInt32, (Read Only) The number of turns the order will take</li>
