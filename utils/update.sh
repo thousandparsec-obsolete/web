f5471abb5484ee53ade76a7919bec532a468c2d0
Tim Ansell <tim@thousandparsec.net>
2004-05-02 17:28:34 +0000

    Now generates python documentation.

--- utils/update.sh
+++ utils/update.sh
@@ -5,7 +5,15 @@ CVSROOT="/var/www/www.thousandparsec.net/cvs/"
 cd $CVSROOT
 cvs update -dP
 
+cd $CVSROOT/web/dev/documents/python/
+
+epydoc --html -o netlib-doc -v --private-css green netlib
+tar -zcvf py-netlib-doc-cvs.tar.gz netlib-doc/*
+
 # Run the sloc2html update
 cd $CVSROOT/web/utils
 
 exec ./sloc2html.sh
+
+
+
