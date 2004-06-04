98ecbf0038f12dab902b577d957abb8289ba4876
Tim Ansell <tim@thousandparsec.net>
2004-06-04 16:12:01 +0000

    Should show time of posting on the news page now...

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ $files = get_files($news);
 
 foreach($files as $file) {
 	include "bits/start_section.inc";
+?>
+	<td>Posted: <?php echo substr($file, 0, -5); ?></td>
+<?
 	include($news . $file);
 	include "bits/end_section.inc";
 }
