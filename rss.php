9b134f0d4ea18e6902fe65d055ac7b34782c1512
Tim Ansell <tim@thousandparsec.net>
2004-07-13 17:24:37 +0000

    Fixed rss stuff..

--- rss.php
+++ rss.php
@@ -1,3 +1,6 @@
+<?php header('Content-type: application/rss+xml'); ?>
+<?php include "bits/func.inc" ; ?>
+<?php echo '<?xml version="1.0" encoding="iso-8859-1"?>' ?>
 <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
   <channel>
     <title>Thousand Parsec News</title>
@@ -19,14 +22,22 @@ foreach($files as $file) {
 	# Extract the author
 	preg_match_all("|<p>by (.*?)</p>|i",$haystack,$author);
 
-	$body = strip_tags( $haystack );
+	$haystack = str_replace(array($title[0][0], $author[0][0]), "", $haystack);
+	$haystack = preg_replace('!<a.*?href\s*=[\'"\s]*(.*?)[\'"\s]*>(.*?)</a>!', '\2 - http://www.thousandparsec.net/tp/\1', $haystack);
+	$haystack = str_replace(array("/tp//tp/", "/./"), array("/tp/", "/"), $haystack);
+	$haystack = preg_replace('!\s*\n\s*!', "\n", $haystack);
+	$haystack = strip_tags( $haystack );
+	$haystack = preg_replace('!\n\n+!', "\n\n", $haystack);
+	$body = $haystack;
+
+#	print_r($title);
 ?>
     <item>
-      <title><?php echo $title ?></title>
+      <title><?php echo $title[1][0] ?></title>
       <link>http://www.thousandparsec.net/tp/</link>
       <description><?php echo $body ?></description>
-      <dc:creator><?php echo $author ?></dc:creator>
-      <dc:date><?php echo substr($file, 0, -5); ?></dc:date>    
+      <dc:creator><?php echo $author[1][0] ?></dc:creator>
+      <dc:date><?php echo substr($file, 0, -10); ?>T<?php echo substr($file, -9, -7); ?>:<?php echo substr($file, -7, -5); ?>:00-00:00</dc:date>    
     </item>
 <? } ?>
   </channel>
