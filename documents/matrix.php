54fca4b4b5844ddd8dba84bb863d5c162d5a3b77
Lee Begg <lee@thousandparsec.net>
2004-10-31 14:46:00 +0000

    Updating the libtpproto-cpp features (finally, just about everything works).

--- documents/matrix.php
+++ documents/matrix.php
@@ -87,14 +87,14 @@
 		<td align="center" bgcolor="#00ff00">#</td>
 		<td align="center" bgcolor="#00ff00">#</td>
 		<td align="center" bgcolor="#ff0000">#</td>
-		<td align="center" bgcolor="#ff0000">#</td>
+		<td align="center" bgcolor="#ffff00">#</td>
 	</tr>
 	<tr>
 		<td>Can remove orders?</td>
 		<td align="center" bgcolor="#00ff00">#</td>
 		<td align="center" bgcolor="#00ff00">#</td>
 		<td align="center" bgcolor="#ff0000">#</td>
-		<td align="center" bgcolor="#ff0000">#</td>
+		<td align="center" bgcolor="#00ff00">#</td>
 	</tr>
 	<tr>
 		<td>Can view boards?</td>
