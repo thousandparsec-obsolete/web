22f7fba5bb0fffb8c890d675d0323d283f5ec60e
Lee Begg <lee@thousandparsec.net>
2004-11-04 14:47:52 +0000

    Libtpproto-cpp can post messages.
    libtpproto-cpp can post messages.

--- documents/matrix.php
+++ documents/matrix.php
@@ -137,7 +137,7 @@
 		<td align="center" bgcolor="#ff0000">#</td>
 		<td align="center" bgcolor="#ff0000">#</td>
 		<td align="center" bgcolor="#ff0000">#</td>
-		<td align="center" bgcolor="#ffff00">#</td>
+		<td align="center" bgcolor="#00ff00">#</td>
 		<td align="center" bgcolor="#ff0000">#</td>
 	<tr>
 		<td>Can tell when a turn will end?</td>
