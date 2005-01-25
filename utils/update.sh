306b4633d132e2a2bb65c4c1ee08389b70a3a819
Lee Begg <lee@thousandparsec.net>
2005-01-25 04:47:27 +0000

    Now builds libtpproto-cpp docs and doc tarball.

--- utils/update.sh
+++ utils/update.sh
@@ -10,6 +10,14 @@ cd $CVSROOT/web/dev/documents/python/
 epydoc --html -o netlib-doc -v --private-css green --docformat plaintext netlib
 tar -zcvf py-netlib-doc-cvs.tar.gz netlib-doc/*
 
+#create libtpproto-cpp docs.
+cd $CVSROOT/libtpproto-cpp/
+doxygen Doxfile
+mv docs libtpproto-cpp-doc
+tar czf libtpproto-cpp-doc-cvs.tar.gz libtpproto-cpp-doc
+mv libtpproto-cpp-doc-cvs.tar.gz libtpproto-cpp-doc/html/
+
+
 # Run the sloc2html update
 cd $CVSROOT/web/utils
 
