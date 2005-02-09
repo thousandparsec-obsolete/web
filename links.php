5cb3346eacb965da7934c597164f0de01e3fe0b9
Tim Ansell <tim@thousandparsec.net>
2005-02-09 10:03:56 +0000

    Added outer space and free orion to the links page.

--- links.php
+++ links.php
@@ -17,6 +17,35 @@
 
 <p>
 	<h2>
+		Similar Open Source Games
+	</h2>
+</p>
+<ul>
+	<li>
+		<a href="http://www.ospace.net">
+			Outer Space by K2 Software.
+		</a>
+		<p>
+			Outer Space is a python+pygame based game which is fully playable.
+		</p>
+	</li>
+	<li>
+		<a href="http://www.freeorion.org">
+			Free Orion.
+		</a>
+		<p>
+			Free Orion is aiming to create a clone of the popular Master of Orion game
+			series.
+		</p>
+	</li>
+</ul>
+
+
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<p>
+	<h2>
 		Universe links
 	</h2>
 </p>
@@ -45,7 +74,9 @@
 </p>
 <ul>
 	<li>
-		Way point Zero, offical Stars! home page
+		<a href="http://www.crisium.com/stars/index.html">
+			Way point Zero, offical Stars! home page
+		</a>
 	</li>
 </ul>
 
