62b9f7491d201bf6f60ab6775bfb50771896ebf0
Tim Ansell <tim@thousandparsec.net>
2005-02-22 06:01:50 +0000

    Updates for the rename...

--- utils/update.sh
+++ utils/update.sh
@@ -5,10 +5,10 @@ CVSROOT="/var/www/thousandparsec/cvs/"
 cd $CVSROOT
 cvs update -dP
 
+#create py-netlib docs.
 cd $CVSROOT/web/dev/documents/python/
-
-epydoc --html -o netlib-doc -v --private-css green --docformat plaintext netlib
-tar -zcvf py-netlib-doc-cvs.tar.gz netlib-doc/*
+epydoc --html -o libtpproto-py-doc -v --private-css green --docformat plaintext netlib
+tar -zcvf libtpproto-py-doc-cvs.tar.gz libtpproto-py-doc/*
 
 #create libtpproto-cpp docs.
 cd $CVSROOT/libtpproto-cpp/
@@ -18,11 +18,7 @@ mv docs libtpproto-cpp-doc
 tar czf libtpproto-cpp-doc-cvs.tar.gz libtpproto-cpp-doc
 mv libtpproto-cpp-doc-cvs.tar.gz libtpproto-cpp-doc/html/
 
-
 # Run the sloc2html update
 cd $CVSROOT/web/utils
-
 exec ./sloc2html.sh
-
-
 
