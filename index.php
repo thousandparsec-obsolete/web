9d693f49f812d4bce6a167a58fbc6480c40873fc
Tim Ansell <tim@thousandparsec.net>
2006-06-29 23:16:41 +0000

    Boxes on the side of the page.

--- index.php
+++ index.php
@@ -2,6 +2,36 @@
 <?php include "bits/start_page.inc" ; ?>
 <?php include "bits/func.inc" ; ?>
 
+<div id="boxes">
+<?php include(dirname(__FILE__) . "/tmp/fm-stats.inc"); ?>
+<?php include(dirname(__FILE__) . "/tmp/sf-stats.inc"); ?>
+<div style="
+	background-repeat: no-repeat;
+	background-image: url(/tp/tmp/fm-stats-small.png);
+	background-position: right center;">
+	<b class="small"><a href="http://freshmeat.net/project-stats/view/43366/">Freshmeat Stats:</a></b><br>
+	<span class="small">Rating:</span>     <?php echo $fm_rating; ?>/10.00<br>
+	<span class="small">Vitality:</span>   <?php echo $fm_vitality_percent; ?>%,
+	<span class="small">Rank</span>        <?php echo $fm_vitality_rank; ?><br>
+	<span class="small">Popularity:</span> <?php echo $fm_popularity_percent; ?>%, 
+	<span class="small">Rank</span>        <?php echo $fm_popularity_rank; ?><br>
+</div><div>
+	<b class="small"><a href="http://sourceforge.net/project/stats/?group_id=132078&ugn=thousandparsec">SourceForge Stats:</a></b><br>
+	<span class="small">Bugs:</span> <?php echo $sf_bugs_open . "/" . $sf_bugs_closed; ?><br>
+	<span class="small">Todo:</span> <?php echo $sf_todo_open . "/" . $sf_todo_closed; ?><br>
+	<span class="small">Devs:</span> <?php echo $sf_devs; ?><br>
+<!--
+</div><div>
+	<b class="small">New Posts to ML:</b><br>
+	<br>
+</div><div>
+	<b class="small">Latest Developments:</b><br>
+	<br>
+-->
+</div>
+</div>
+
+<div style="padding-right: 220px;">
 <?php
 
 $news = "news/";
@@ -9,6 +39,8 @@ $files = get_files($news);
 
 $i = 0;
 foreach($files as $file) {
+	if ($i == 2) echo "</div>\n";
+
 	if ($i > 10)
 		break;
 	else
@@ -18,7 +50,7 @@ foreach($files as $file) {
 	include($news . $file);
 ?>
 	<p class="small">Posted: <?php echo substr($file, 0, -5); ?></p>
-<?
+<?php
 	include "bits/end_section.inc";
 }
 
--- utils/rss-get.sh
+++ utils/rss-get.sh
@@ -1,16 +1,31 @@
 #! /bin/sh
 
-DARCSROOT="/var/www/thousandparsec/repos/"
-cd $DARCSROOT
+if [ "x$DARCSROOT" = "x" ]; then
+	DARCSROOT="/var/www/thousandparsec/repos/"
+fi
 
+cd $DARCSROOT
 TMP=$DARCSROOT/tmp
 
 # Download the sourceforge rss details (for bugs, etc)
-wget "http://sourceforge.net/export/rss2_projsummary.php?group_id=132078&go" -O $TMP/sf.rss > /dev/null 2>&1
+wget "http://sourceforge.net/export/rss2_projsummary.php?group_id=132078&go" -O $TMP/sf.rss
 # Post process the rss details
-more $TMP/sf.rss | grep "Tracker: Bugs" | sed -e's/.*: Bugs (\(.*\) open\/\(.*\) total).*/<?php $bugs_open="\1"; $bugs_closed="\2";/' > $TMP/sf-bugs.inc
-more $TMP/sf.rss | grep "Tracker: Todo" | sed -e's/.*: Todo (\(.*\) open\/\(.*\) total).*/<?php $todo_open="\1"; $todo_closed="\2";/' > $TMP/sf-todo.inc
+echo "<?php " > $TMP/sf-stats.inc
+cat $TMP/sf.rss | grep "Tracker: Bugs" | sed -e's/.*: Bugs (\(.*\) open\/\(.*\) total).*/$sf_bugs_open="\1"; $sf_bugs_closed="\2";/' >> $TMP/sf-stats.inc
+cat $TMP/sf.rss | grep "Tracker: Todo" | sed -e's/.*: Todo (\(.*\) open\/\(.*\) total).*/$sf_todo_open="\1"; $sf_todo_closed="\2";/' >> $TMP/sf-stats.inc
+cat $TMP/sf.rss | grep "Developers on project: " | sed -e's/.*Developers on project: \(.*\)<.*/$sf_devs="\1";/' >> $TMP/sf-stats.inc
 
+find $TMP/fm.rss -mtime +1 -exec rm '{}' ';'
+if [ ! -e $TMP/fm.rss ]; then
+	# Download the freshmeat rss details
+	wget "http://freshmeat.net/projects-xml/tp/tp.xml" -O $TMP/fm.rss
+	echo "<?php " > $TMP/fm-stats.inc
+	cat $TMP/fm.rss | egrep -e 'vitality|popularity|rating|subscriptions|url_project_page' | sed -e's|<\(.*\)>\(.*\)</.*>|$fm_\1 = "\2";|' >> $TMP/fm-stats.inc
+
+	# Download the freshmeat graphics
+	wget --user-agent="Mozilla/5.0 lookalike" "http://freshmeat.net/project-stats/import-download-stats-small/43366/" -O $TMP/fm-stats.png
+	convert -crop 25x50+75+0 $TMP/fm-stats.png $TMP/fm-stats-small.png
+fi
 
 # Download the rss details from the darcs repository
 export repositories="
@@ -35,7 +50,7 @@ if [ ! -d $TMP/darcs ]; then
 	mkdir $TMP/darcs
 fi
 for repository in $repositories; do
-	wget "http://darcs.thousandparsec.net/darcsweb/darcsweb.cgi?r=$repository;a=rss" -O $TMP/darcs/$repository.rss > /dev/null 2>&1
+	wget "http://darcs.thousandparsec.net/darcsweb/darcsweb.cgi?r=$repository;a=rss" -O $TMP/darcs/$repository.rss
 done
 
 # Post process the repository rss (combind them, sort them and trim old)
