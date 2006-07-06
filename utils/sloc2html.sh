e06ee4ccbd6c7681d748836907b6f4cf6a4bee66
Lee Begg <lee@thousandparsec.net>
2006-07-06 12:59:58 +0000

    Fix mithro's change.

--- utils/sloc2html.sh
+++ utils/sloc2html.sh
@@ -4,7 +4,7 @@ REPOROOT=/var/www/thousandparsec/repos/
 TMPDIR=/tmp/sloc2html
 
 INPUT=/tmp/sloc.output
-OUTPUT=/var/www/thousandparsec/tp/dev/tmp/sloc.inc
+OUTPUT=/var/www/thousandparsec/tp/dev/bits/sloc.inc
 
 mkdir $TMPDIR
 sloccount --wide --multiproject --datadir $TMPDIR $REPOROOT > $INPUT 2> /dev/null
