cbd625c76825cf59aa46b9377327119d7542b4b6
Tim Ansell <tim@thousandparsec.net>
2006-07-25 14:49:05 +0000

    More IE Fixes...

--- js/pngfix.js
+++ js/pngfix.js
@@ -1,39 +1,47 @@
-/*
- 
-Correctly handle PNG transparency in Win IE 5.5 & 6.
-http://homepage.ntlworld.com/bobosola. Updated 18-Jan-2006.
+// Universal transparent-PNG enabler for MSIE/Win 5.5+
+// http://dsandler.org
+// From original code: http://www.youngpup.net/?request=/snippets/sleight.xml
+// and background-image code: http://www.allinthehead.com/retro/69
+// also:
+//  * use sizingMethod=crop to avoid scaling PNGs (who would do such a thing?)
+//  * only do this once, to make it compatible with CSS rollovers
 
-Use in <HEAD> with DEFER keyword wrapped in conditional comments:
-<!--[if lt IE 7]>
-<script defer type="text/javascript" src="pngfix.js"></script>
-<![endif]-->
+if (navigator.platform == "Win32" && navigator.appName == "Microsoft Internet Explorer" && window.attachEvent) {
+	window.attachEvent("onload", enableAlphaImages);
+}
 
-*/
+function enableAlphaImages(){
+	var rslt = navigator.appVersion.match(/MSIE (\d+\.\d+)/, '');
+	var itsAllGood = (rslt != null && Number(rslt[1]) >= 5.5);
+	if (itsAllGood) {
+		for (var i=0; i<document.all.length; i++){
+			var obj = document.all[i];
+			var bg = obj.currentStyle.backgroundImage;
+			var img = document.images[i];
+			if (bg && bg.match(/\_t.png/i) != null) {
+				var img = bg.substring(5,bg.length-2);
+				var offset = obj.style["background-position"];
 
-var arVersion = navigator.appVersion.split("MSIE")
-var version = parseFloat(arVersion[1])
+				if (obj.style.backgroundRepeat == "no-repeat") {
+					obj.style.filter =
+						"progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+img+"', sizingMethod='crop')";
+				} else { 
+					if (obj.style.width == '')
+						obj.style.width = obj.offsetWidth;	
+					obj.style.filter =
+						"progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+img+"', sizingMethod='scale')";
+				}				
+				obj.style.backgroundImage = "url('/tp/img/spacer.gif')";
+				obj.style["background-position"] = offset; // reapply
+			} else if (img && img.src.match(/\_t.png$/i) != null) {
+				var src = img.src;
+				img.style.width = img.width + "px";
+				img.style.height = img.height + "px";
+				img.style.filter =
+					"progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='crop')"
+				img.src = "/tp/img/spacer.gif";
+			}
 
-if ((version >= 5.5) && (document.body.filters)) 
-{
-   for(var i=0; i<document.images.length; i++)
-   {
-      var img = document.images[i]
-      var imgName = img.src.toUpperCase()
-      if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
-      {
-         var imgID = (img.id) ? "id='" + img.id + "' " : ""
-         var imgClass = (img.className) ? "class='" + img.className + "' " : ""
-         var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
-         var imgStyle = "display:inline-block;" + img.style.cssText 
-         if (img.align == "left") imgStyle = "float:left;" + imgStyle
-         if (img.align == "right") imgStyle = "float:right;" + imgStyle
-         if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle
-         var strNewHTML = "<span " + imgID + imgClass + imgTitle
-         + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
-         + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
-         + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>" 
-         img.outerHTML = strNewHTML
-         i = i-1
-      }
-   }
+		}
+	}
 }
