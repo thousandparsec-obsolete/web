75a64ae06545fadf44e88b30d3469a077fe092ca
Tim Ansell <tim@thousandparsec.net>
2006-09-29 07:30:35 +0000

    Fixup the Links page.

--- links.php
+++ links.php
@@ -6,16 +6,21 @@
 <h2>
 	Thousand Parsec links
 </h2>
+<p>
+	These links are to sites which are directly related to Thousand Parsec.
+</p>
 <ul>
-	<li></li>
+	<li>No links currently available.</li>
 </ul>
-
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/start_section.inc" ?>
-
 <h2>
 	Similar Open Source Games
 </h2>
+<p>
+	These links are to sites which are for open source games with similar themes
+	to Thousand Parsec.
+</p>
 <ul>
 	<li>
 		<a href="http://www.ospace.net">
@@ -34,22 +39,13 @@
 			incorporating novel design ideas.
 		</p>
 	</li>
-</ul>
-
-<?php include "bits/end_section.inc" ?>
-<?php include "bits/start_section.inc" ?>
-
-<h2>
-	Universe links
-</h2>
-<p>
-	Another open source game similar to Thousand Parsec, it's written in JAVA.
-</p>
-<ul>
 	<li>
 		<a href="http://universe.sf.net">
 			Universe at Sourceforge
 		</a>
+		<p>
+			Another open source game similar to Thousand Parsec, it's written in JAVA.
+		</p>
 	</li>
 </ul>
 
@@ -57,51 +53,41 @@
 <?php include "bits/start_section.inc" ?>
 
 <h2>
-	Stars! links
+	Similar Commercial Games
 </h2>
 <p>
-	Stars! is a similar game to what we are trying to create. It's a decent 
-	game and there is a playable demo. 
+	These links are to similar games which are commercial.
 </p>
 <ul>
+	<li>
+		<a href="http://www.galciv2.com/">
+			Galactic Civilizations
+		</a>
+		<p>
+			A series of similar games produced by StarDock! Recently very successful
+			game which has many similarities to what we are trying to create.
+		</p>
+	</li>
+
+
 	<li>
 		<a href="http://www.crisium.com/stars/index.html">
-			Way point Zero, offical Stars! home page
+			Stars! 
 		</a>
+		<p>
+			Stars! is a similar game to what we are trying to create. It's a decent 
+			game and there is a playable demo. Link is the official website, "Waypoint Zero". 
+		</p>
 	</li>
-</ul>
-
-<?php include "bits/end_section.inc" ?>
-<?php include "bits/start_section.inc" ?>
-
-<h2>
-	VGA Planets links
-</h2>
-<p>
-	VGA Planets is another similar game. There is also a shareware version.
-</p>
-<ul>
-	<li></li>
-</ul>
-
-<?php include "bits/end_section.inc" ?>
-<?php include "bits/start_section.inc" ?>
-
-<h2>
-		XPace links
-</h2>
-<p>
-	Another commerical game like Stars! and VGA Planets. Downloadable for
-	Free because it's been discontinued. Was never finished.
-</p>
-<ul>
 	<li>
-		<a href="http://www.xoftware.com">
-			Xoftware
-		</a>, 
-          creators of XPACE
+		<a href="http://www.vgaplanets.com">
+			VGA Planets Home Page
+		</a>
+		<p>
+			VGA Planets is another similar game. There is also a shareware version.
+			Official home page by game's creator Tim Wisseman. Latest releases of hosting and client software.
+		</p>
 	</li>
 </ul>
-
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/end_page.inc" ?>
