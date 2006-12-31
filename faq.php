104a174fb97b934bba9ffd1bbf700130ef18c081
Tim Ansell <tim@thousandparsec.net>
2006-12-31 22:26:44 +0000

    Small updates to FAQ.

--- faq.php
+++ faq.php
@@ -31,30 +31,41 @@
 	</li>
 	<li>
 		Playing Questions
+			<ol>
+				<li>
+					<a href="#3-1">
+						I found a bug!
+					</a>
+				</li>
+			</ol>
 	</li>
 	<li>
 		Extending Questions
+			<ol>
+				<li>
+					<a href="#4-1">
+						How can I contact you?
+					</a>
+				</li>
+			</ol>
 	</li>
 </ol>
 
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/start_section.inc" ?>
 
+<a name="1-1"></a>
+<h2>1.1 What is Thousand Parsec?</h2>
 <p>
-	<a name="1-1"></a>
-	1.1 What is Thousand Parsec?
-</p>
-<p>
-	Thousand Parsec is a turn based space empire building game.
+	Thousand Parsec is a turn based space empire building game. You can find out 
+more information on the <a href="/tp/gettingstarted.php">Getting Started</a> page.
 </p>
 
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/start_section.inc" ?>
 
-<p>
-	<a name="1-2"></a>
-	1.2 Is Thousand Parsec related to <a href="http://www.parsec.org">Parsec?</a>
-</p>
+<a name="1-2"></a>
+<h2>1.2 Is Thousand Parsec related to <a href="http://www.parsec.org">Parsec?</a></h2>
 <p>
 	No. They are completely different games.
 </p>
@@ -62,12 +73,55 @@
 <?php include "bits/end_section.inc" ?>
 <?php include "bits/start_section.inc" ?>
 
-<p>
-	<a name="1-3"></a>1.3 TP for my Bunghole!
-</p>
+<a name="1-3"></a>
+<h2>1.3 TP for my Bunghole!</h2>
 <p>
 	We are in no way related to Bevis and Butthead. The show sucks, get over it already.
 </p>
 
 <?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<a name="3-1"></a>
+<h2>3.1 I found a bug!</h2>
+<p>
+Please report the bug using the <a href="http://sourceforge.net/tracker/?group_id=132078&atid=723099">Sourceforge Bug tracker.</a>.
+If you don't think you can use the bug tracker just send an email to the
+<a href="/tp/mailman.php/listinfo/">mailing lists</a> or post on the <a href="/forums">forums</a>.
+</p><p>
+Telling us on IRC will increase the likelyhood of the bug getting forgotten!
+</p>
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/start_section.inc" ?>
+
+<a name="4-1"></a>
+<h2>4.1 How can I contact you?</h2>
+<p>
+You have a choice of 3 options, you can email us on the 
+<a href="/tp/mailman.php/listinfo/">mailing lists</a>. You can post to the <a href="/forums">forums</a>
+or you can join us on <a href="irc://irc.freenode.org/#tp">IRC</a>.
+</p>
+<?php include "bits/end_section.inc" ?>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
 <?php include "bits/end_page.inc" ?>
