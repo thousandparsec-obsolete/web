a116edde741e852f5a0e724837149a4b885f0f0e
Tim Ansell <tim@thousandparsec.net>
2007-01-05 12:30:14 +0000

    Flash Intro.

---
+++ intro.php
@@ -0,0 +1,23 @@
+<html>
+<body bgcolor="#000000">
+
+<div style="text-align: center">
+<object type="application/x-shockwave-flash" data="graphics/FlowPlayerLight.swf" 
+	width="400" height="300" id="FlowPlayer">
+	<param name="allowScriptAccess" value="sameDomain" />
+	<param name="movie" value="FlowPlayer.swf" />
+	<param name="quality" value="high" />
+	<param name="scale" value="noScale" />
+	<param name="wmode" value="transparent" />
+	<param name="flashvars" value="config={
+		useEmbeddedButtonImages: false,
+		hideControls: true,
+		videoFile: 'intro.flv',
+		loop: false,
+}" />
+</div>
+</object>
+<a href="index.php">
+</a>
+</body>
+</html>
