7bb1044d21b68e733f08aa5e7906e7f5b5765099
Jure Repinc <jlp@holodeck1.com>
2007-03-15 10:37:01 +0000

    Initial Google Summer of Code page.

---
+++ google-summer-of-code-2007.php
@@ -0,0 +1,67 @@
+<?php $title = "Google Summer of Code 2007" ?>
+<?php include "../bits/start_page.inc" ?>
+<?php include "../bits/start_section.inc" ?>
+
+<h1>Google Summer of Code 2007</h1>
+<h6>Last updated: 15 March 2007</h6>
+
+<p>Thousand Parsec is one of the mentoring organisations for Google Summer of Code 2007.
+This is a great chance for all students to work on a cool open source game project during
+the summer of 2007, have fun and earn some money during the process.</p>
+
+<h2>Ideas</h2>
+
+<p>Below are some of the ideas and suggestion for students who are interested in joining
+Thousand Parsec project. The list is not definitive and any student is more than welcome
+to come up with their own idea to work on.</p>
+
+<h3>Implement MTSec game in one server</h3>
+<p>Currently Thousand Parsec servers only implement a very simple game we call MiniSec.
+To make the game more interesting and challenging a more complex game needs to be implemented
+in any of the servers. The rules and properties of this new game are already defined in a
+game we call MTSec (short for Missile and Torpedo Wars). For more information see
+<a href="/tp/dev/documents/mtsec.php">MTSec documentation</a>.</p>
+
+<h3>Web based client</h3>
+<p>Web based client enables players to join Thousand Parsec games easily from any computer
+with a web browser and internet connection. This makes web based client the fastest and
+easiest way to start playing the game.</p>
+
+<h3>Improve the XSLT document generation</h3>
+
+<h3>Improve the metaserver - make it standalone</h3>
+
+<p>For additional ideas we encourage students to look at
+<a href="http://sourceforge.net/tracker/?group_id=132078&atid=829724">Thousand Parsec TODO list</a>.</p>
+
+<h2>Important dates</h2>
+<dl>
+
+<dt>March 14</dt>
+<dd>List of accepted mentoring organizations published on code.google.com; student application period opens</dd>
+
+<dt>March 24</dt>
+<dd>Student application deadline</dd>
+
+<dt>April 9</dt>
+<dd>List of accepted student applications published on code.google.com</dd>
+ 
+<dt>May 28</dt>
+<dd>Students begin coding for their GSoC projects; Google begins issuing initial student payments</dd>
+ 
+<dt>July 9</dt>
+<dd>Students upload code to code.google.com/hosting; mentors begin mid-term evaluations</dd>
+ 
+<dt>July 16</dt>
+<dd>Mid-term evaluation deadline; Google begins issuing mid-term student payments</dd>
+ 
+<dt>August 20</dt>
+<dd>Students upload code to code.google.com/hosting; mentors begin final evaluations; students begin final program evaluations</dd>
+ 
+<dt>August 31</dt>
+<dd>Final evaluation deadline; Google begins issuing student and mentoring organization payments</dd>
+
+</dl>
+
+<?php include "../bits/end_section.inc" ?>
+<?php include "../bits/end_page.inc" ?>
