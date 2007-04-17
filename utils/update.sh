91fc8043d8748cb48f56e62341aac880dfe05efa
Tim Ansell <tim@thousandparsec.net>
2007-04-17 20:13:20 +0000

    S/DARCSROOT/GITROOT/.

--- utils/update.sh
+++ utils/update.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-DARCSROOT="/var/www/thousandparsec/repos/"
+GITROOT="/var/www/thousandparsec/repos/"
 
 #create the stable version of tp.* documents.
 cd $DARCSROOT/web/dev/documents/python/netlib
@@ -14,21 +14,24 @@ epydoc --html -o python-tp-doc -v --private-css green --docformat plaintext tp
 tar -zcvf python-tp-doc.tar.gz python-tp-doc/*
 
 #create libtpproto-cpp docs.
-cd $DARCSROOT/libtpproto-cpp/
+cd $GITROOT/libtpproto-cpp/
+cg-update
 doxygen Doxyfile
 cd ..
 tar czf libtpproto-cpp-doc-cvs.tar.gz libtpproto-cpp/docs/
-rm -fr $DARCSROOT/web/dev/documents/libtpproto-cpp/*
-mv libtpproto-cpp-doc-cvs.tar.gz libtpproto-cpp/docs/* $DARCSROOT/web/dev/documents/libtpproto-cpp/
+rm -fr $GITROOT/web/dev/documents/libtpproto-cpp/*
+mv libtpproto-cpp-doc-cvs.tar.gz libtpproto-cpp/docs/* $GITROOT/web/dev/documents/libtpproto-cpp/
 
 # Create any new thumbnails
-cd $DARCSROOT/web/screenshots
+cd $GITROOT/web/screenshots
 ./makethumbs.sh
 
 #generate content of protocolxml.php from darcs
-xsltproc $DARCSROOT/web/dev/documents/protocol2html.xsl $DARCSROOT/documents/protocol/protocol.xml > $DARCSROOT/web/dev/bits/protocolxml.inc
+cd $GITROOT/documents
+cg-update
+xsltproc $GITROOT/web/dev/documents/protocol2html.xsl ./protocol/protocol.xml > $GITROOT/web/dev/bits/protocolxml.inc
 
 # Run the sloc2html update
-cd $DARCSROOT/web/utils
+cd $GITROOT/web/utils
 exec ./sloc2html.sh
 
