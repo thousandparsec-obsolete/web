1e5d9a5a0d3f2eacc2bd32358f887e09c7a2acc7
Tim Ansell <mithro@mithis.com>
2007-04-25 22:46:25 +0930

    Uppercase tags are not valid in XHTML.

--- index.php
+++ index.php
@@ -118,7 +118,7 @@
 </div>
 
 <div class="stats" style="text-align: center">
-<SCRIPT type='text/javascript' language='JavaScript' src='http://www.ohloh.net/projects/3679;badge_js'></SCRIPT>
+<script type='text/javascript' language='JavaScript' src='http://www.ohloh.net/projects/3679;badge_js'></script>
 </div>
 
 <?php include "bits/end_section.inc"; ?> 
