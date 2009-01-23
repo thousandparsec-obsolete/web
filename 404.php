155d56f8b1a437b4829fd4ec5a60b9a9b90f8f00
Tim Ansell <mithro@mithis.com>
2009-01-23 12:25:28 +1100

    Added a custom 404 error page.

---
+++ 404.php
@@ -0,0 +1,46 @@
+<?php $title = "Not found!" ?>
+<?php include "bits/start_page.inc" ; ?>
+
+<style type="text/css">
+
+#goog-wm-sb {
+	margin: 2px;
+	background: #000000;
+	border: 1px solid white;
+	color: #ffffff;
+}
+
+#goog-wm-qt {
+	margin: 2px;
+	background: #000000;
+	border: 1px solid white;
+	color: #ffffff;
+}
+</style>
+
+<div style="height: 5px;"></div>
+
+<?php include "bits/start_section.inc" ?>
+
+<h1>Sorry we could not find the requested location!</h1>
+
+<h3>Here are some helpful links:</h3>
+
+<ul>
+	<li>Don't know what Thousand Parsec is or want more information? <a href="/tp/gettingstarted.php">Start Here</a></li>
+</ul>
+
+<script type="text/javascript">
+  var GOOG_FIXURL_LANG = 'en';
+  var GOOG_FIXURL_SITE = 'http://www.thousandparsec.net/';
+</script>
+<script type="text/javascript" 
+    src="http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
+
+</p>
+
+</div>
+<?php include "bits/end_section.inc" ?>
+<?php include "bits/end_page.inc" ; ?>
+
+
